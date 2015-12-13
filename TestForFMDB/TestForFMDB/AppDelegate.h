acc27dc64914e5342ac5c1f9fc0d1351deecfce7
xueyingbo <xueyingbozz@sina.com>
2015-12-13 10:27:40 +0800

    新建项目

---
+++ TestForFMDB/TestForFMDB/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  TestForFMDB
+//
+//  Created by 薛迎波 on 15/12/13.
+//  Copyright © 2015年 XueYingbo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
